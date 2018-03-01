b39c562f23926d2cc63cf2bd7915dbea71763138
RobertSellers <robert.sellers@azimuth1.com>
2018-02-28 21:34:08 -0500

    update gpx validation

--- R/gpx_validation.R
+++ R/gpx_validation.R
@@ -1,11 +1,5 @@
-gpx_validation <- function(mipfile, ...){
-  if(substring(tolower(mipfile), nchar(mipfile)-3) != ".gpx"){
+gpx_validation <- function(gpxfile, ...){
+  if(substring(tolower(gpxfile), nchar(gpxfile)-3) != ".gpx"){
     stop('Uploaded data needs to be .zip file. ');
-  }else{
-    fname = unzip(mipfile, list=TRUE)$Name
-    mhp_filename = fname[grep(".mhp", fname)]
-    if(identical(mhp_filename, character(0))){
-      stop('Zip file does not contain .mhp file');
-    }
   }
 }
