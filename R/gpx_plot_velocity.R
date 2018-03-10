2e0a7f161e77e796ab106e55d74ad26119b37d83
RobertSellers <robert.sellers@azimuth1.com>
2018-03-10 11:26:04 -0500

    attempt namespace error / mongolite

--- R/gpx_plot_velocity.R
+++ R/gpx_plot_velocity.R
@@ -1,29 +1,12 @@
-gpx_validation <- function(gpxfile, ...){
-  if(substring(tolower(gpxfile), nchar(gpxfile)-3) != ".gpx"){
-    stop('Uploaded data needs to be .zip file. ');
-  }else{
-    options(stringsAsFactors = FALSE)
-    require(plotKML)
-    tryCatch({
-      data <- readGPX(gpxfile, metadata = TRUE, bounds = TRUE, 
-                    waypoints = TRUE, tracks = TRUE, routes = TRUE)
-    }, error = function(e){
-      stop(cat(paste("GPX loading error", e)))
-    }, finally = {
-      return (data)
-    })
-  }
-}
+gpx_plot_velocity <- function(gpxfile){
 
-gpx_plot_velocity <- function(mipfile){
-
-  if(substring(tolower(mipfile), nchar(mipfile)-3) != ".zip"){
+  if(substring(tolower(gpxfile), nchar(gpxfile)-3) != ".zip"){
     stop('Uploaded data needs to be a .zip file. ');
   }
 
   tryCatch({
 
-    plot_title = substr(basename(mipfile),0,nchar(basename(mipfile))-8)
+    plot_title = substr(basename(gpxfile),0,nchar(basename(gpxfile))-8)
     p1<-ggplot(data, aes(x = data$"Depth (ft)")) +
       geom_line(aes(y = data$"Hydrostatic Pressure (psi)", color = "Hydrostatic Pressure"), linetype = "longdash") +
       geom_line(aes(y = data$"HPT Press. Avg (psi)", color="HPT Press. Avg (psi)")) +
@@ -31,7 +14,7 @@ gpx_plot_velocity <- function(mipfile){
       scale_color_manual(values = c("red","lightblue","black","blue" )) +
       scale_y_continuous(breaks=seq(0,max(data$"HPT Press. Avg (psi)",na.rm=TRUE),10)) +
       scale_x_continuous(breaks=seq(0,max(data$"Depth (ft)",na.rm=TRUE),10)) +
-      ggtitle(basename(gsub('.{4}$', '', mipfile))) +
+      ggtitle(basename(gsub('.{4}$', '', gpxfile))) +
       xlab("Depth (ft)") +
       ylab("Pressure (psi)") +
       coord_flip()  +
