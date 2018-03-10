a9d5353cb4aba4da6d13c1d2453b777fb535b701
RobertSellers <robert.sellers@azimuth1.com>
2018-03-10 14:29:48 -0500

    RobertSellers

--- read_gpx_test.R
+++ read_gpx_test.R
@@ -1,4 +1,6 @@
 require(plotKML)
-test_gpx<-'C:/Users/rober/Desktop/gpx_processing/data/14-05.gpx'
+
+
+test_gpx<-'C:/Users/rober/Desktop/gpx_processing/data_backup/14-05.gpx'
 data_test<-readGPX(test_gpx, metadata = TRUE, bounds = TRUE, 
                    waypoints = TRUE, tracks = TRUE, routes = TRUE)
