5bed4d4530ede555879f911d3a665569d7630a9a
RobertSellers <robert.sellers@azimuth1.com>
2018-04-18 10:29:43 -0400

    testing initialization

--- inst/www/initialize.js
+++ inst/www/initialize.js
@@ -1,30 +1,31 @@
-function getScripts(scripts, callback) {
-    var progress = 0;
-    scripts.forEach(function(script) { 
-        $.getScript(script, function () {
-            if (++progress == scripts.length) callback();
-        }); 
+function loadLibraries(){
+    function getScripts(scripts, callback) {
+        var progress = 0;
+        scripts.forEach(function(script) { 
+            $.getScript(script, function () {
+                if (++progress == scripts.length) callback();
+            }); 
+        });
+    }
+    return getScripts(
+        [
+        "scripts/opencpu-0.4.js", 
+        "scripts/jqueryui/jquery-ui-1.10.3.custom.js",
+        "scripts/knockout-min.js",
+        "scripts/leaflet.js",
+        "scripts/gpx.js",
+        "scripts/jquery.dataTables.min.js",
+        "scripts/dataTables.buttons.min.js",
+        "scripts/dataTables.flash.min.js",
+        "scripts/pdfmake.min.js",
+        "scripts/vfs_font.js",
+        "scripts/buttons.html5.min.js",
+        "scripts/buttons.print.min.js",
+        "https://code.highcharts.com/highcharts.js",
+        "https://code.highcharts.com/modules/series-label.js",
+        "https://code.highcharts.com/modules/exporting.js",
+        "https://code.highcharts.com/modules/export-data.js"
+        ], function () {
+            callback(true);  
     });
 }
-
-getScripts(
-    [
-    "scripts/opencpu-0.4.js", 
-    "scripts/jqueryui/jquery-ui-1.10.3.custom.js",
-    "scripts/knockout-min.js",
-    "scripts/leaflet.js",
-    "scripts/gpx.js",
-    "scripts/jquery.dataTables.min.js",
-    "scripts/dataTables.buttons.min.js",
-    "scripts/dataTables.flash.min.js",
-    "scripts/pdfmake.min.js",
-    "scripts/vfs_font.js",
-    "scripts/buttons.html5.min.js",
-    "scripts/buttons.print.min.js",
-    "https://code.highcharts.com/highcharts.js",
-    "https://code.highcharts.com/modules/series-label.js",
-    "https://code.highcharts.com/modules/exporting.js",
-    "https://code.highcharts.com/modules/export-data.js"
-    ], function () {
-        callback(true);  
-});
