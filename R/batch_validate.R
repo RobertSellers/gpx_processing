6981009e238eaed47d6e2b3913b5006ba6ce3442
RobertSellers <robert.sellers@azimuth1.com>
2018-07-27 13:39:27 -0600

    batchin processing

--- R/batch_validate.R
+++ R/batch_validate.R
@@ -1,4 +1,33 @@
 batch_validate <- function(header, ...){
     myfiles <- list(...) 
-    return (myfiles)
+    num_files <- length(myfiles)
+    response <- data.frame(
+        error=character(num_files),
+        validate_response=character(num_files),
+        stringsAsFactors = FALSE
+    )
+    for (i in 1:num_files){
+        if(substring(tolower(gpx_files[i]), nchar(gpx_files[i]) - 3) != ".gpx"){
+            response$error[i] <- "Improper Data Format"
+            response$validate_response[i] <- "Error"
+        }else{
+            tryCatch({
+                data <- plotKML::readGPX(
+                    gpx_files[i], 
+                    metadata = TRUE, 
+                    bounds = TRUE, 
+                    waypoints = TRUE, 
+                    tracks = TRUE, 
+                    routes = TRUE
+                )
+            }, error = function(e){
+                response$validate_response[i] <- "Error"
+                response$error[i] <- e
+            }, finally = {
+                response$validate_response[i] <- "Success"
+                response$error[i] <- "none"
+            })
+        }
+    }
+    return (response)
   }
