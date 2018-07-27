d157278e6f4f85874fdbd8fd574fb75644860a58
RobertSellers <robert.sellers@azimuth1.com>
2018-07-27 16:00:00 -0600

    hacky nonsense

--- R/batch_validate.R
+++ R/batch_validate.R
@@ -6,16 +6,39 @@ batch_validate <- function(header, ...){
         validate_response=character(num_files),
         stringsAsFactors = FALSE
     )
-for (item in gpx_files){
-    test<-plotKML::readGPX(
-        item, 
-        metadata = TRUE, 
-        bounds = TRUE, 
-        waypoints = TRUE, 
-        tracks = TRUE, 
-        routes = TRUE
-    )
-}
+    for (item in gpx_files){
+        test<-plotKML::readGPX(
+            item, 
+            metadata = TRUE, 
+            bounds = TRUE, 
+            waypoints = TRUE, 
+            tracks = TRUE, 
+            routes = TRUE
+        )
+    }
     
     return (test)
   }
+
+  message <- function (..., domain = NULL, appendLF = TRUE) 
+    {
+        args <- list(...)
+        cond <- if (length(args) == 1L && inherits(args[[1L]], "condition")) {
+            if (nargs() > 1L) 
+                warning("additional arguments ignored in message()")
+            args[[1L]]
+        }
+        else {
+            msg <- .makeMessage(..., domain = domain, appendLF = appendLF)
+            call <- sys.call()
+            simpleMessage(msg, call)
+        }
+        defaultHandler <- function(c) {
+            cat(conditionMessage(c), file = stdout(), sep = "")
+        }
+        withRestarts({
+            signalCondition(cond)
+            defaultHandler(cond)
+        }, muffleMessage = function() NULL)
+        invisible()
+    }
