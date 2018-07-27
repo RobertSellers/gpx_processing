e2bb566a1cacc806e820efc6d98798eec229b473
RobertSellers <robert.sellers@azimuth1.com>
2018-07-27 14:27:41 -0600

    batch

--- R/batch_validate.R
+++ R/batch_validate.R
@@ -2,8 +2,8 @@ batch_validate <- function(header, ...){
     gpx_files <- list(...) 
     num_files <- length(gpx_files)
     response <- data.frame(
-        error=character(),
-        validate_response=character(),
+        error=character(num_files),
+        validate_response=character(num_files),
         stringsAsFactors = FALSE
     )
     test <- "NONE"
@@ -26,5 +26,5 @@ batch_validate <- function(header, ...){
             response$error[i] <- e
         })
     }
-    return (response,i)
+    return (response)
   }
