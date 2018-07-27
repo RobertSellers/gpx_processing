3f6ab13339967974aedeeb2292f47c252fe9a00b
RobertSellers <robert.sellers@azimuth1.com>
2018-07-27 10:16:50 -0600

    batch validate system p4

---
+++ R/batch_validate.R
@@ -0,0 +1,36 @@
+batch_validate <- function(gpx_files, ...){
+  
+    response <- data.frame(
+        error=character(),
+        validate_response=character(),
+        stringsAsFactors = FALSE
+    )
+
+    i <- 1
+    for (gpx in gpx_files){
+        if(substring(tolower(gpx), nchar(gpx)-3) != ".gpx"){
+            response$error[i] <- "Improper Data Format"
+            response$validate_response[i] <- "Error"
+        }else{
+            tryCatch({
+                data <- plotKML::readGPX(
+                    gpx_files, 
+                    metadata = TRUE, 
+                    bounds = TRUE, 
+                    waypoints = TRUE, 
+                    tracks = TRUE, 
+                    routes = TRUE
+                )
+            }, error = function(e){
+                response$validate_response[i] <- "Error"
+                response$error[i] <- e
+            }, finally = {
+                response$validate_response[i] <- "Success"
+                response$error[i] <- "none"
+                return (response)
+            })
+        }
+        i<-i+1
+    }
+  }
+}
