fb443b16a937b1b3ac3f782ecf468f6accffbee2
RobertSellers <robert.sellers@azimuth1.com>
2018-07-27 14:31:00 -0600

    batch

--- R/batch_validate.R
+++ R/batch_validate.R
@@ -7,24 +7,15 @@ batch_validate <- function(header, ...){
         stringsAsFactors = FALSE
     )
     test <- "NONE"
-    for (i in 1:num_files){
-        tryCatch({
-            test <- TRUE
+
             plotKML::readGPX(
-                gpx_files[i], 
+                gpx_files[1], 
                 metadata = TRUE, 
                 bounds = TRUE, 
                 waypoints = TRUE, 
                 tracks = TRUE, 
                 routes = TRUE
             )
-            response$validate_response[i] <- "Success"
-            response$error[i] <- "none"
-        }, error = function(e){
-            test <- FALSE
-            response$validate_response[i] <- "Error"
-            response$error[i] <- e
-        })
     }
     return (response)
   }
