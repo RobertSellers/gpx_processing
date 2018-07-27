22aab52a3d90d5a52f36a4fc557dbfa3d1ac1928
RobertSellers <robert.sellers@azimuth1.com>
2018-07-27 17:07:22 -0600

    kill me part 5

--- R/batch_validate.R
+++ R/batch_validate.R
@@ -3,52 +3,23 @@ batch_validate <- function(header, ...){
     num_files <- length(gpx_files)
     validate_response <- c()
     error_log<-c()
-    saveRDS(gpx_files,file = "data.Rdata")
+    df_list <- list()
     i <- 1
     for (item in gpx_files){
 # #
         tryCatch({
-            data_test<-plotKML::readGPX(
-                item, 
-                metadata = TRUE, 
-                bounds = TRUE, 
-                waypoints = TRUE, 
-                tracks = TRUE, 
-                routes = TRUE
-            )
-                validate_response[i] <- "Success"
-                error_log[i] <- "none"
+            df_list[[i]] <- gpx_df_construct(gpxfile)
+            validate_response[i] <- "Success"
+            error_log[i] <- "none"
             }, error = function(e){
                 validate_response[i] <- "Error"
                 error_log[i] <- e
+                df_list[[i]] <- NULL
             }, finally = {
                 #nothing
 
         })
         i <- i + 1
     }
-    return (list("response"=validate_response,"error"=error_log))
+    return (df_list)
   }
-
-  message <- function (..., domain = NULL, appendLF = TRUE) 
-    {
-        args <- list(...)
-        cond <- if (length(args) == 1L && inherits(args[[1L]], "condition")) {
-            if (nargs() > 1L) 
-                warning("additional arguments ignored in message()")
-            args[[1L]]
-        }
-        else {
-            msg <- .makeMessage(..., domain = domain, appendLF = appendLF)
-            call <- sys.call()
-            simpleMessage(msg, call)
-        }
-        defaultHandler <- function(c) {
-            cat(conditionMessage(c), file = stdout(), sep = "")
-        }
-        withRestarts({
-            signalCondition(cond)
-            defaultHandler(cond)
-        }, muffleMessage = function() NULL)
-        invisible()
-    }
