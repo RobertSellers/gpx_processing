a076d7aa4229b4569939daf0c96eaef07b81ea99
RobertSellers <robert.sellers@azimuth1.com>
2018-07-30 16:11:51 -0600

    index updated

--- inst/www/viewModel.js
+++ inst/www/viewModel.js
@@ -16,9 +16,7 @@ var vm = {
     get_elevation_loss_imp: ko.observable(),
     selectedIndex:ko.observable(),
     elevation_net: ko.observable(),
-    updateVisualizations: function(){
-        // TBD
-    },
+
     downloadCSV : function() {
         var data = this.rDataOutput();
         var filename = this.gpxFileName().slice(0, -4);
